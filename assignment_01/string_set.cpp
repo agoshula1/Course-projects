e4f3a2febc45b67d77ed3cb9b4e18f889199a1d8
agoshula1 <agoshula1@users.noreply.github.com>
2016-06-13 18:49:02 -0700

    Added descriptive comments

--- assignment_01/string_set.cpp
+++ assignment_01/string_set.cpp
@@ -1,5 +1,7 @@
 /*
- * 
+ * Program Name: String Set
+ * Program Description: A class that stores a set of strings in each instance.
+ * Includes operations for adding, removing, and checking for membership of a string in a set.
  */
 
 #include <iostream>
@@ -11,21 +13,29 @@
 
 using namespace std;
 
+//initialize the set
 string_set::string_set() {
+	//make the set empty to begin with
 	for(int i = 0; i < HASH_TABLE_SIZE; i++){
             hash_table[i] = NULL;
     	}
 	
+	//start iterator at first index
 	iterator_index = 0;
+	
+	//no element yet in first index
 	iterator_node = NULL; 
 }
 
+//create copy of a set
 string_set::string_set(string_set &s0){
 	node *p;
 	
+	//fill up new set with same strings
 	for(int i = 0; i < HASH_TABLE_SIZE; i++){
 		p = s0.hash_table[i];
 		
+		//go through linked list in each slot of the original table
 		while(p != NULL){
 			add(p->s);
 			p = p->next;
@@ -34,7 +44,9 @@ string_set::string_set(string_set &s0){
 	
 }
 
+//add s to the set
 void string_set::add(const char *s) {
+	//s is already in the set
 	if(contains(s) == 1){
 		throw duplicate_exception();
 
@@ -57,18 +69,24 @@ void string_set::add(const char *s) {
 				//adds the string to the set
 				strcpy(p->s, s);
 				
+				//place s in appropriate slot
 				int index = hash_function(s);
 				p->next = hash_table[index];
 				hash_table[index] = p;
 				
+				//reset the iterator after insertion
 				reset();
 			}
 		}
 	}
 }
 
+//remove s from the set
 void string_set::remove(const char *s) {
+	//find slot where s should be
 	int index = hash_function(s);
+	
+	//var to indicate if s has been found
 	bool found = false;
 	
 	node *pre = hash_table[index];	
@@ -92,6 +110,7 @@ void string_set::remove(const char *s) {
 					found = true;
 
 				}else{				
+					//still looking for s
 					pre = cur;
 					cur = cur->next;
 				}
@@ -102,29 +121,35 @@ void string_set::remove(const char *s) {
 		throw not_found_exception();
 
 	}else{		
+		//reset the iterator after removal
 		reset();
 	}
 }
 
+//checks for membership of s in the set
 int string_set::contains(const char *s) {
+	//find slot where s should be
 	int i = hash_function(s);
 
+    //go through liked list of the slot s hashes to
     for(node *p = hash_table[i]; p != NULL; p = p->next){
         if(strcmp(s, p->s) == 0){
-            //the string already exists in the set
+        		 //the string already exists in the set
 			return 1;
         }
     }
 	
-	//the string does not exist in the set
+    //the string does not exist in the set
     return 0;
 }
 
+//reset the iterator to the first element
 void string_set::reset() {
 	iterator_index = 0;
 	iterator_node = hash_table[iterator_index];
 }
 
+//return a pointer to the next element
 const char *string_set::next() {
 	node *p;
 	
@@ -139,16 +164,18 @@ const char *string_set::next() {
 		return NULL;
 	}
 	
-	//returns next string and points to next node
+	//returns next string and points iterator to next node
 	p = iterator_node;
 	iterator_node = iterator_node->next;
 	return p->s;
 }
 
+//delete dynamically allocated memory
 string_set::~string_set() {
 	node *p1;
 	node *p2;
 	
+	//go through each slot of the table to delete memory
 	for(int i = 0; i < HASH_TABLE_SIZE; i++){
 		if(hash_table[i] != NULL){
 			p1 = hash_table[i];
@@ -164,13 +191,15 @@ string_set::~string_set() {
 	}		
 }
 
+//calculate hash value of s
 int string_set::hash_function(const char *s) {
     int h = 0;
 	
-	//adds up the ASCII values of each character in the string
+    //adds up the ASCII values of each character in the string
     for(int i = 0; s[i] != '\0'; i++){            
 			h += s[i];
     }
-
+    
+    //make sure 0 <= h < HASH_TABLE_SIZE
     return h % HASH_TABLE_SIZE;
 }
